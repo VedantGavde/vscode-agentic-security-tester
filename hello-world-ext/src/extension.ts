708e8b11dfaafcacb1ed16b2a8bc7329bf656714
VedantGavde <wfebou1998@gmail.com>
2025-08-15 03:18:45 -0400

    Added Java AST parsing into VS Code extension

--- hello-world-ext/src/extension.ts
+++ hello-world-ext/src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import * as cp from 'child_process';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.parseJava', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showErrorMessage('No active editor');
+            return;
+        }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hello-world-ext" is now active!');
+        const filePath = editor.document.fileName;
+        if (!filePath.endsWith('.java')) {
+            vscode.window.showErrorMessage('The active file is not a Java file.');
+            return;
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hello-world-ext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Hello World Extension!');
-	});
+        // Path to the java-parser folder where JavaParserCLI.class and jars exist
+        const parserFolder = path.join(context.extensionPath, 'java-parser');
 
-	context.subscriptions.push(disposable);
+        // Build the java command to run your CLI
+        const cmd = `java -cp ".:${path.join(parserFolder, '*')}" JavaParserCLI "${filePath}"`;
+
+        // Run the java command
+        cp.exec(cmd, { cwd: parserFolder }, (err, stdout, stderr) => {
+            if (err) {
+                vscode.window.showErrorMessage(`Error: ${stderr || err.message}`);
+                return;
+            }
+
+            // Show AST in Output panel
+            const outputChannel = vscode.window.createOutputChannel('Java AST');
+            outputChannel.clear();
+            outputChannel.append(stdout);
+            outputChannel.show();
+        });
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
+
